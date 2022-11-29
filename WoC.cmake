ab899333c0830754101dbc8a3246b2ef45c3237b
AnchyDev <lewienato@gmail.com>
2022-11-30 08:42:11 +1100

    Fix cmake file path.

--- WoC.cmake
+++ WoC.cmake
@@ -1,5 +1,5 @@
 set(WOC_MODULE_DIR ${CMAKE_SOURCE_DIR}/modules/${SOURCE_MODULE})
-set(WOC_MODULE_SRC_DIR ${WASM_MODULE_DIR}/src)
+set(WOC_MODULE_SRC_DIR ${WOC_MODULE_DIR}/src)
 
 target_include_directories(modules PUBLIC ${WOC_MODULE_SRC_DIR}/Config)
 target_link_directories(modules PUBLIC ${WOC_MODULE_SRC_DIR}/Config)
