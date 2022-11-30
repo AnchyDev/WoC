5622ae768951ed21787914d120eb327381cb9038
AnchyDev <lewienato@gmail.com>
2022-11-30 16:22:53 +1100

    Added item composer creature boilerplate

---
+++ src/Components/ItemComposer/WoCICCreatureScript.cpp
@@ -0,0 +1,1 @@
+#include "WoCICCreatureScript.h"
---
+++ src/Components/ItemComposer/WoCICCreatureScript.h
@@ -0,0 +1,16 @@
+#ifndef MODULE_WOC_IC_CREATURESCRIPT_H
+#define MODULE_WOC_IC_CREATURESCRIPT_H
+
+#include "ScriptMgr.h"
+#include "Player.h"
+#include "Chat.h"
+
+class WoCICCreatureScript : public CreatureScript
+{
+public:
+    WoCICCreatureScript() : CreatureScript("WoCICCreatureScript") { }
+private:
+    
+};
+
+#endif //MODULE_WOC_IC_CREATURESCRIPT_H
