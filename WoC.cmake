5c1d6364a08eae312622aada818d5f4d910e9524
AnchyDev <lewienato@gmail.com>
2022-11-30 12:50:36 +1100

    Changed target_sources PRIVATE->PUBLIC.

--- WoC.cmake
+++ WoC.cmake
@@ -1,7 +1,6 @@
 set(WOC_MODULE_DIR ${CMAKE_SOURCE_DIR}/modules/${SOURCE_MODULE})
 set(WOC_MODULE_SRC_DIR ${WOC_MODULE_DIR}/src)
 
-target_include_directories(modules PUBLIC ${WOC_MODULE_SRC_DIR}/Config)
-target_include_directories(modules PUBLIC ${WOC_MODULE_SRC_DIR}/Components)
-
-target_sources(modules PRIVATE ${WOC_MODULE_SRC_DIR}/Config/WoCConfig.cpp ${WOC_MODULE_SRC_DIR}/Components/ItemGenerator/WoCIGPlayerScript.cpp) 
+target_sources(modules PUBLIC 
+	${WOC_MODULE_SRC_DIR}/Config/WoCConfig.cpp 
+	${WOC_MODULE_SRC_DIR}/Components/ItemGenerator/WoCIGPlayerScript.cpp) 
