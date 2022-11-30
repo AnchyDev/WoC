d8456eb90a8ad892ac20f70af28de295f3f19bbf
AnchyDev <lewienato@gmail.com>
2022-11-30 12:31:26 +1100

    Add log message.

--- WoC.cmake
+++ WoC.cmake
@@ -1,7 +1,7 @@
 set(WOC_MODULE_DIR ${CMAKE_SOURCE_DIR}/modules/${SOURCE_MODULE})
 set(WOC_MODULE_SRC_DIR ${WOC_MODULE_DIR}/src)
 
-message(STATUS "CMAKESOURCEDIR: ${CMAKE_SOURCE_DIR}")
+message(STATUS "CMAKESOURCEDIR: ${WOC_MODULE_DIR}")
 
 target_include_directories(modules PUBLIC ${WOC_MODULE_SRC_DIR}/Config)
 target_include_directories(modules PUBLIC ${WOC_MODULE_SRC_DIR}/Components)
